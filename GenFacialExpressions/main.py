5877c0bd39ef01bf6b7040b11c8c56e0f05dc2ee
bod3 <78141647+bodejan@users.noreply.github.com>
2023-11-27 17:34:59 -0600

    update labels, create final dataset

--- GenFacialExpressions/main.py
+++ GenFacialExpressions/main.py
@@ -9,7 +9,7 @@ import torch
 from Classifier.classifier import classify_emotion
 from StyleGan.gen_img_v2 import gen_stylegan3_img_v2
 from StyleGan.gen_img_v3 import gen_stylegan3_img_v3
-from emotion_vector import FEAR, DISGUST, ANGRY, SURPRISE, SAD, HAPPY, NEUTRAL
+from model import FEAR, DISGUST, ANGRY, SURPRISE, SAD, HAPPY, NEUTRAL
 
 
 # Add the StyleGAN3 folder to Python path
@@ -58,23 +58,57 @@ def save_to_two_csvs(name, z, label):
 
 #main(100, 900)
 
-def gen_avg():
-    gen_stylegan3_img_v3(ANGRY, 'ANGRY')
-    gen_stylegan3_img_v3(DISGUST, 'DISGUST')
-    gen_stylegan3_img_v3(FEAR, 'FEAR')
-    gen_stylegan3_img_v3(SURPRISE, 'SURPRISE')
-    gen_stylegan3_img_v3(SAD, 'SAD')
-    gen_stylegan3_img_v3(NEUTRAL, 'NEUTRAL')
-    gen_stylegan3_img_v3(HAPPY, 'HAPPY')
+def gen_avg_vectors():
+    angry = gen_stylegan3_img_v3(ANGRY, 'ANGRY')
+    disgust = gen_stylegan3_img_v3(DISGUST, 'DISGUST')
+    fear = gen_stylegan3_img_v3(FEAR, 'FEAR')
+    surprise = gen_stylegan3_img_v3(SURPRISE, 'SURPRISE')
+    sad = gen_stylegan3_img_v3(SAD, 'SAD')
+    neutral = gen_stylegan3_img_v3(NEUTRAL, 'NEUTRAL')
+    happy = gen_stylegan3_img_v3(HAPPY, 'HAPPY')
 
-#gen_avg()
+    # Plotting
+    fig, axs = plt.subplots(1, 7, figsize=(20, 5))
+    axs[0].imshow(angry)
+    axs[0].set_title(f'Angry avg. vector.')
+    axs[0].axis('off')
 
-def gen_mix(plt_name, factor=0.80):
+    axs[1].imshow(disgust)
+    axs[1].set_title(f'Disgust avg. vector.')
+    axs[1].axis('off')
+
+    axs[2].imshow(fear)
+    axs[2].set_title(f'Fear avg. vector.')
+    axs[2].axis('off')
+
+    axs[3].imshow(surprise)
+    axs[3].set_title(f'Surprise avg. vector.')
+    axs[3].axis('off')
+
+    axs[4].imshow(sad)
+    axs[4].set_title(f'Sad avg. vector.')
+    axs[4].axis('off')
+
+    axs[5].imshow(neutral)
+    axs[5].set_title(f'Neutral avg. vector.')
+    axs[5].axis('off')
+
+    axs[6].imshow(happy)
+    axs[6].set_title(f'Happy avg. vector.')
+    axs[6].axis('off')
+
+    plt.savefig('avg_vectors', bbox_inches='tight')
+    plt.close(fig)
+
+#gen_avg_vectors()
+
+def gen_mix(plt_name, factor=0.75):
     def add_and_multiply_lists(list_a, list_b, factor):
         return [a * (1-factor) + b * factor for a, b in zip(list_a, list_b)]
     
     random = torch.randn(512)
     random_img = gen_stylegan3_img_v3(random, f'Random, factor: {factor}')
+    random_img.save(f'img/result/random/random_{plt_name}.png')
 
     neutral_base = add_and_multiply_lists(random, NEUTRAL, factor)
 
@@ -88,12 +122,14 @@ def gen_mix(plt_name, factor=0.80):
         else:
             emotion_input = add_and_multiply_lists(random, emotion_factor, factor)
         emotion_img = gen_stylegan3_img_v3(emotion_input, f'{emotions[i]}, factor: {factor}')
+        # save img
+        emotion_img.save(f'img/result/{emotions[i].lower()}/{emotions[i].lower()}_{plt_name}.png')
         images.append(emotion_img)
 
     # Plotting
     fig, axs = plt.subplots(1, len(images) + 1, figsize=(20, 5))
     axs[0].imshow(random_img)
-    axs[0].set_title(f'Random, factor: {factor}')
+    axs[0].set_title(f'Random')
     axs[0].axis('off')
     for i, img in enumerate(images):
         axs[i + 1].imshow(img)
@@ -101,10 +137,10 @@ def gen_mix(plt_name, factor=0.80):
         axs[i + 1].axis('off')
 
     #plt.show()
-    plt.savefig(plt_name, bbox_inches='tight')
+    plt.savefig(f'img/result/row/row_{plt_name}.png', bbox_inches='tight')
     plt.close(fig)
 
 
-for i in range(0, 20):
+for i in range(0, 50):
     gen_mix(str(i))
 
