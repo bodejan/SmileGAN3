d7574ff4f0ed354889550aa6ffdc94e09a7c8824
bod3 <78141647+bodejan@users.noreply.github.com>
2023-11-28 13:55:41 -0600

    plot results

--- visualize.py
+++ visualize.py
@@ -1,6 +1,8 @@
 
 import time
 import matplotlib.pyplot as plt
+from PIL import Image
+import os
 
 from smilegan3 import gen_img_for_vector
 from model import FEAR, DISGUST, ANGRY, SURPRISE, SAD, HAPPY, NEUTRAL
@@ -52,3 +54,35 @@ def visualize_avg_vectors(export = False):
     if export:
         time.sleep(5)
         plt.close(fig)
+
+def create_image_grid(folder_path, output_path = None, grid_size=(10, 10), image_size=(100, 100)):
+    images = []
+
+    # Load 100 images from the folder
+    for filename in os.listdir(folder_path)[:100]:
+        if filename.endswith(".jpg") or filename.endswith(".png"):
+            img = Image.open(os.path.join(folder_path, filename))
+            img = img.resize(image_size)  # Resize if necessary
+            images.append(img)
+
+    # Create a new image with the appropriate size
+    grid_img = Image.new('RGB', (image_size[0] * grid_size[0], image_size[1] * grid_size[1]))
+
+    # Paste the images into the grid
+    for i, img in enumerate(images):
+        grid_x = i % grid_size[0] * image_size[0]
+        grid_y = i // grid_size[0] * image_size[1]
+        grid_img.paste(img, (grid_x, grid_y))
+    
+    grid_img.show()
+    if output_path:
+        grid_img.save(output_path)
+
+    return grid_img
+
+def plot_all_emotions():
+    emotions = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise', 'random']
+    for emotion in emotions:
+        create_image_grid(f'img/result/{emotion}/', f'img/result/{emotion}/{emotion}_faces.png')
+
+    create_image_grid(f'img/result/row/', f'img/result/row/all_faces.png', (4, 25), (800, 100))
